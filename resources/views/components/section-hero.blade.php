fb1d20f9e967f3dbd9469e53e94b4a8acc118efc
mkhotamirais <mkhotamirais@gmail.com>
2025-04-25 19:09:44 +0700

    update home:hero-2

--- resources/views/components/section-hero.blade.php
+++ resources/views/components/section-hero.blade.php
@@ -1,13 +1,13 @@
 @props([
     'title' => __('common.home.hero.title'),
-    'height' => 'h-[36vh]',
+    'height' => 'h-[40vh]',
     'banner' => 'storage/img/unsplash_home_hero_bg.avif',
     'total' => 0,
 ])
 
 <section id="hero" class="relative {{ $height }} w-full bg-gradient-to-b from-black/30 to-black/60">
     <img src="{{ asset($banner) }}" alt="panoramaalam hero" width="100%" height="100%"
-        class="absolute w-full object-cover object-center h-full -z-10">
+        class="absolute w-full object-cover object-center {{ $height }} -z-10">
     <div class="container text-white h-full flex items-center w-full">
         <div class="max-w-4xl leading-relaxed w-full">
             <h1 class="text-4xl lg:text-6xl font-semibold capitalize">{{ $title }}
