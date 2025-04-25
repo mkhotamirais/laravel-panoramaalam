98fccb54b08914857cc229684ac8dbfab023c379
mkhotamirais <mkhotamirais@gmail.com>
2025-04-25 20:15:05 +0700

    balikin image

--- resources/views/components/section-hero.blade.php
+++ resources/views/components/section-hero.blade.php
@@ -6,8 +6,8 @@
 ])
 
 <section id="hero" class="relative {{ $height }} w-full bg-gradient-to-b from-black/30 to-black/60">
-    {{-- <img src="{{ asset($banner) }}" alt="panoramaalam hero" loading="lazy"
-        class="absolute w-full object-cover object-center {{ $height }} -z-10"> --}}
+    <img src="{{ asset($banner) }}" alt="panoramaalam hero" loading="lazy"
+        class="absolute w-full object-cover object-center {{ $height }} -z-10">
     <div class="container text-white h-full flex items-center w-full">
         <div class="max-w-4xl leading-relaxed w-full">
             <h1 class="text-4xl lg:text-6xl font-semibold capitalize">{{ $title }}
